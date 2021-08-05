1541037f2569308fcde416cbc6969816baa4a267
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-08-05 22:27:38 +0200

    add a BufReader fork
    std::io::BufReader implements BufRead::fill_buf by only refilling if
    there is no more data in the buffer. For nom parsers, where we might
    need more data to parse (example: there are 2 bytes in the buffer, we
    want to read a u32), we must be able to drive the refill properly

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ use futures::{
     AsyncRead,
 };
 
+pub mod bufreader;
+
 #[derive(Debug)]
 pub enum Error<E> {
     Error(E),
@@ -29,6 +31,39 @@ pub trait Parse<O, E, P> {
 }
 
 impl<R: Read, O, E, P> Parse<O, E, P> for std::io::BufReader<R> {
+    fn parse(&mut self, mut p: P) -> Result<O, Error<E>>
+    where
+        for<'a> P: Parser<&'a [u8], O, E>,
+    {
+        loop {
+            let opt =
+                    //match p(input.buffer()) {
+                    match p.parse(self.buffer()) {
+                        Err(Err::Error(e)) => return Err(Error::Error(e)),
+                        Err(Err::Failure(e)) => return Err(Error::Failure(e)),
+                        Err(Err::Incomplete(_)) => {
+                            None
+                        },
+                        Ok((i, o)) => {
+                            let offset = self.buffer().offset(i);
+                            Some((offset, o))
+                        },
+                };
+
+            match opt {
+                Some((sz, o)) => {
+                    self.consume(sz);
+                    return Ok(o);
+                }
+                None => {
+                    self.fill_buf()?;
+                }
+            }
+        }
+    }
+}
+
+impl<R: Read, O, E, P> Parse<O, E, P> for bufreader::BufReader<R> {
     fn parse(&mut self, mut p: P) -> Result<O, Error<E>>
     where
         for<'a> P: Parser<&'a [u8], O, E>,
