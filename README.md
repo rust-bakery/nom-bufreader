493fd60d80ec1bba5997df3ec9421b1c883538cd
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-08-21 12:46:58 +0200

    update docs, prepare for nom 7.0

--- README.md
+++ README.md
@@ -8,6 +8,71 @@ and [futures::io::BufReader](https://docs.rs/futures/0.3.16/futures/io/struct.Bu
 cannot be used directly. This crate proovide compatible forks instead, in the
 `bufreader` and `async_bufreader` modules.
 
-It will hide for you the [Incomplete](https://docs.rs/nom/6.2.1/nom/enum.Err.html#variant.Incomplete) handling in nom for streaming parsers, retrying and refilling buffers automatically.
+It will hide for you the [Incomplete](https://docs.rs/nom/7.0.0/nom/enum.Err.html#variant.Incomplete) handling in nom for streaming parsers, retrying and refilling buffers automatically.
 
-See the `examples/` directory for usage
+## Examples
+
+### sync
+
+```rust
+use nom_bufreader::bufreader::BufReader;
+use nom_bufreader::{Error, Parse};
+use std::{net::TcpListener, str::from_utf8};
+
+fn main() -> Result<(), Error<()>> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let mut i = BufReader::new(listener.incoming().next().unwrap()?);
+
+    // method, space and path are nom parsers
+    let m = i.parse(method)?;
+    let _ = i.parse(space)?;
+    let p = i.parse(path)?;
+    println!("got method {}, path {}", m, p);
+    Ok(())
+}
+```
+
+### async
+
+#### tokio
+
+```rust
+use nom_bufreader::async_bufreader::BufReader;
+use nom_bufreader::{AsyncParse, Error};
+use std::str::from_utf8;
+use tokio_util::compat::TokioAsyncReadCompatExt;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Error<()>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let mut i = BufReader::new(listener.accept().await?.0.compat());
+
+    let m = i.parse(method).await?;
+    let _ = i.parse(space).await?;
+    let p = i.parse(path).await?;
+    println!("got method {}, path {}", m, p);
+    Ok(())
+}
+```
+
+#### async-std
+
+```rust
+use nom_bufreader::async_bufreader::BufReader;
+use nom_bufreader::{AsyncParse, Error};
+use std::str::from_utf8;
+use async_std::net::TcpListener;
+
+#[async_std::main]
+async fn main() -> Result<(), Error<()>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let mut i = BufReader::new(listener.accept().await?.0);
+
+    let m = i.parse(method).await?;
+    let _ = i.parse(space).await?;
+    let p = i.parse(path).await?;
+    println!("got method {}, path {}", m, p);
+    Ok(())
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,77 @@
 //! This crate provides a `BufReader` alternative that can be used with
 //! [nom parsers](http://docs.rs/nom)
 //!
-//! # Example
-//!
-//! ```rust,ignore
-//! let listener = TcpListener::bind("127.0.0.1:8080")?;
-//! let mut i = BufReader::new(listener.incoming().next().unwrap()?);
-//!
-//! // method, space and path are all nom parsers
-//! let m = i.parse(method)?;
-//! let _ = i.parse(space)?;
-//! let p = i.parse(path)?;
-//! println!("got method {}, path {}", m, p);
-//! ```
+//! It will hide for you the [Incomplete](https://docs.rs/nom/7.0.0/nom/enum.Err.html#variant.Incomplete) handling in nom for streaming parsers, retrying and refilling buffers automatically.
 //!
 //! For synchronous io, use `bufreader::BufReader`, while for asynchronous
 //! IO, you should use `async_bufreader::BufReader`
+//!
+//! # Examples
+//!
+//! ## sync
+//!
+//! ```rust,no_run
+//! use nom_bufreader::bufreader::BufReader;
+//! use nom_bufreader::{Error, Parse};
+//! use std::{net::TcpListener, str::from_utf8};
+//!
+//! fn main() -> Result<(), Error<()>> {
+//!     let listener = TcpListener::bind("127.0.0.1:8080")?;
+//!     let mut i = BufReader::new(listener.incoming().next().unwrap()?);
+//!
+//!     // method, space and path are nom parsers
+//!     let m = i.parse(method)?;
+//!     let _ = i.parse(space)?;
+//!     let p = i.parse(path)?;
+//!     println!("got method {}, path {}", m, p);
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ### async
+//!
+//! #### tokio
+//!
+//! ```rust,no_run
+//! use nom_bufreader::async_bufreader::BufReader;
+//! use nom_bufreader::{AsyncParse, Error};
+//! use std::str::from_utf8;
+//! use tokio_util::compat::TokioAsyncReadCompatExt;
+//! use tokio::net::TcpListener;
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<(), Error<()>> {
+//!     let listener = TcpListener::bind("127.0.0.1:8080").await?;
+//!     let mut i = BufReader::new(listener.accept().await?.0.compat());
+//!
+//!     let m = i.parse(method).await?;
+//!     let _ = i.parse(space).await?;
+//!     let p = i.parse(path).await?;
+//!     println!("got method {}, path {}", m, p);
+//!     Ok(())
+//! }
+//! ```
+//!
+//! #### async-std
+//!
+//! ```rust,no_run
+//! use nom_bufreader::async_bufreader::BufReader;
+//! use nom_bufreader::{AsyncParse, Error};
+//! use std::str::from_utf8;
+//! use async_std::net::TcpListener;
+//!
+//! #[async_std::main]
+//! async fn main() -> Result<(), Error<()>> {
+//!     let listener = TcpListener::bind("127.0.0.1:8080").await?;
+//!     let mut i = BufReader::new(listener.accept().await?.0);
+//!
+//!     let m = i.parse(method).await?;
+//!     let _ = i.parse(space).await?;
+//!     let p = i.parse(path).await?;
+//!     println!("got method {}, path {}", m, p);
+//!     Ok(())
+//! }
+//! ```
 use nom::{Err, Offset, Parser};
 use std::io::{self, BufRead, Read};
 
